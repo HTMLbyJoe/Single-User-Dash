15b46e016f552b92fa1cd81a575df6054f4c6e7f
Joe Anzalone <Joe@Shmit.com>
2014-08-03 19:30:09 -0400

    Add routes for tag pages and permalinks

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,34 @@ app.controller('DashboardCtrl', ['$scope', '$routeParams', 'Blog', function($sco
     });
 }]);
 
+app.controller('DashboardCtrl.tag', ['$scope', '$routeParams', 'Blog', function($scope, $routeParams, Blog){
+    var dash = this;
+
+    var blogName    = $routeParams.blogName,
+        tag         = $routeParams.tag;
+    $scope.blogName = blogName;
+    $scope.tag      = tag;
+
+    Blog.query({blogName: blogName, tag: tag}, function(response){
+        $scope.posts = response.posts;
+        $scope.blog  = response.blog;
+    });
+}]);
+
+app.controller('DashboardCtrl.permalink', ['$scope', '$routeParams', 'Blog', function($scope, $routeParams, Blog){
+    var dash = this;
+
+    var blogName    = $routeParams.blogName,
+        postId      = $routeParams.id;
+    $scope.blogName = blogName;
+    $scope.postId   = postId;
+
+    Blog.query({blogName: blogName, id: postId}, function(response){
+        $scope.posts = response.posts;
+        $scope.blog  = response.blog;
+    });
+}]);
+
 app.filter('trusted', ['$sce', function($sce){
     return function(text) {
         return $sce.trustAsHtml(text);
@@ -19,6 +47,9 @@ app.filter('trusted', ['$sce', function($sce){
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-  $routeProvider.when('/:blogName', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl'});
+    $locationProvider.html5Mode(true);
+    $routeProvider.when('/:blogName/post?/:id/:slug?', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl.permalink'});
+    $routeProvider.when('/:blogName/:id/:slug?', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl.permalink'});
+    $routeProvider.when('/:blogName/tagged/:tag', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl.tag'});
+    $routeProvider.when('/:blogName', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl'});
 }]);
