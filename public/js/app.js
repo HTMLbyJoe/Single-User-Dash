44385140515be4f8cdb2cccf4b4546e8935a90e7
Joe Anzalone <Joe@Shmit.com>
2014-08-05 11:16:45 -0400

    Refactor away redundant controllers

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,11 @@
 var app = angular.module('singleUserDash', ['ngRoute', 'ngResource', 'ngSanitize', 'infinite-scroll', 'singleUserDash.services']);
 
 app.controller('DashboardCtrl', ['$scope', '$routeParams', 'PostsPager', function($scope, $routeParams, PostsPager){
-    var dash = this;
+    $scope.blogName = $routeParams.blogName;
 
-    var blogName    = $routeParams.blogName;
-    $scope.blogName = blogName;
-
-    var params   = {blogName: blogName};
-    $scope.pager = new PostsPager(params);
-}]);
-
-app.controller('DashboardCtrl.tag', ['$scope', '$routeParams', 'PostsPager', function($scope, $routeParams, PostsPager){
-    var dash = this;
-
-    var blogName    = $routeParams.blogName,
-        tag         = $routeParams.tag;
-    $scope.blogName = blogName;
-    $scope.tag      = tag;
-
-    var params   = {blogName: blogName, tag: tag};
-    $scope.pager = new PostsPager(params);
-}]);
-
-app.controller('DashboardCtrl.permalink', ['$scope', '$routeParams', 'PostsPager', function($scope, $routeParams, PostsPager){
-    var dash = this;
-
-    var blogName    = $routeParams.blogName,
-        postId      = $routeParams.id;
-    $scope.blogName = blogName;
-    $scope.postId   = postId;
-
-    var params   = {blogName: blogName, id: postId};
+    var params   = {blogName: $routeParams.blogName};
+    params.tag   = $routeParams.tag ? $routeParams.tag : '';
+    params.id    = $routeParams.id  ? $routeParams.id  : '';
     $scope.pager = new PostsPager(params);
 }]);
 
@@ -42,8 +17,8 @@ app.filter('trusted', ['$sce', function($sce){
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
-    $routeProvider.when('/:blogName/post?/:id/:slug?', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl.permalink'});
-    $routeProvider.when('/:blogName/tagged/:tag', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl.tag'});
-    $routeProvider.when('/:blogName/:id/:slug?', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl.permalink'});
+    $routeProvider.when('/:blogName/post?/:id/:slug?', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl'});
+    $routeProvider.when('/:blogName/tagged/:tag', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl'});
+    $routeProvider.when('/:blogName/:id/:slug?', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl'});
     $routeProvider.when('/:blogName', {templateUrl: '/views/posts/list.html', controller: 'DashboardCtrl'});
 }]);
